0703d74764f6e7ab836bb220c13e123ff13152cb
Waseem Shammut <wshammout@gmail.com>
2023-01-05 21:24:44 +0300

    Wrote the assignment requirement

--- Testing.py
+++ Testing.py
@@ -1,3 +1,8 @@
+# Requirement 1: 65%
+# Write a program that prompts the user to input a number then a message should appear that tells the user whether the input number is prime or not.
+# Requirement 2: 35%
+# Another message should appear telling the user what are the factors if the number is not prime.
+
 strX = input('Enter a value for X ')
 x = int(strX)
 factors = []
@@ -11,7 +16,7 @@ for i in range(x + 1): # iteration x number of times
             elif x == i: # last iteration is x equal to i
                 if len(factors) == 0: # if no factor were added previously, that means x is prime
                     result = str(x) + ' is a prime number'
-
 print(result) # requirement 1
+
 if len(factors) != 0:
     print('Factors : ', factors[:]) # requirement 2
